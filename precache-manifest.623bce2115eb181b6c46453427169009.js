2de4ad873c8f1166b218dc2f298d93435ff35d1e
Helen Pang <helenlpang@outlook.com>
2020-03-26 17:44:55 -0400

    Updates

---
+++ precache-manifest.623bce2115eb181b6c46453427169009.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7419f3ad1a8c88b51f43d4a3219a48f1",
+    "url": "/aacf-website/index.html"
+  },
+  {
+    "revision": "e89389ade54d6dd35c59",
+    "url": "/aacf-website/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "7582f34876f58593f925",
+    "url": "/aacf-website/static/css/main.e43b9935.chunk.css"
+  },
+  {
+    "revision": "e89389ade54d6dd35c59",
+    "url": "/aacf-website/static/js/2.8a6e163e.chunk.js"
+  },
+  {
+    "revision": "7582f34876f58593f925",
+    "url": "/aacf-website/static/js/main.eab59866.chunk.js"
+  },
+  {
+    "revision": "db817f5d23bd2255ef81",
+    "url": "/aacf-website/static/js/runtime-main.99d8efb9.js"
+  },
+  {
+    "revision": "49d37d902e39bad7386bbca2bc2c145c",
+    "url": "/aacf-website/static/media/hraacf-logo.49d37d90.png"
+  }
+]);
